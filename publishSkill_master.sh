cbfab4945044cdb6bb045ea421efeb3c94d9901a
Sebastien Coste <teddy.coste@gmail.com>
2017-11-15 11:09:44 +0100

    update config

--- publishSkill_master.sh
+++ publishSkill_master.sh
@@ -3,7 +3,7 @@ cd lambda
 npm install
 zip -r ../lambda.zip *
 cd ..
-aws lambda update-function-configuration --function-name AlexaOblic --description "give an oblic strategy" --role arn:aws:iam::398750413546:role/LambdaFullAccess --handler indexSkill.handler --timeout 10 --memory-size 1024 --environment Variables="{ALEXA_APP_ID=amzn1.ask.skill.b6e260ca-3a10-4b38-b4fd-d2c704f9b1ef,EVENT_TABLE=OblicStrategyEvent,SESSION_TABLE=OblicStrategy}" --profile alexa
+aws lambda update-function-configuration --function-name AlexaOblic --description "give an oblic strategy" --role arn:aws:iam::398750413546:role/LambdaFullAccess --handler indexSkill.handler --timeout 10 --memory-size 1024 --environment Variables="{ALEXA_APP_ID=amzn1.ask.skill.d5e00b10-d4d6-4411-ad33-f857b52d6dbe,EVENT_TABLE=OblicStrategyEvent,SESSION_TABLE=OblicStrategy}" --profile alexa
 aws lambda update-function-code --function-name AlexaOblic --zip-file fileb://lambda.zip --profile alexa
 ask deploy --target skill
 ask deploy --target model
